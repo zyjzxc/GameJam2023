19673247a591cdc55f77c66ffb0e8b9a752a13c0
Jay Zhou <9798870+zyjzxc@users.noreply.github.com>
2023-07-09 08:56:39 +0800

    moving

--- light/Assets/Scripts/Behaviours/Moving.cs
+++ light/Assets/Scripts/Behaviours/Moving.cs
@@ -7,10 +7,13 @@ public class Moving : MonoBehaviour
     [SerializeField] public Vector3 endPosition = new Vector3(0, 10, 0);  // 终点
     [SerializeField] public float moveSpeed = 1.0f;  // 移动速度
     [SerializeField] public float pauseTime = 1.0f;  // 到达终点后的停留时间
+    [SerializeField] public bool movingWith = false;
 
     private float timer = 0.0f;  // 计时器
     private bool movingToTarget = true;  // 是否在向终点移动
     private Vector3 originalPosition;
+    private Rigidbody2D rigidbody2D;
+    private Vector2 lastSpeed;
 
     private Vector3 direction;
 
@@ -21,6 +24,8 @@ public class Moving : MonoBehaviour
     {
         // 开始时在起点
         originalPosition = transform.position;
+        lastSpeed = Vector3.zero;
+        rigidbody2D = null;
     }
 
     private void Update()
@@ -53,6 +58,26 @@ public class Moving : MonoBehaviour
                 movingToTarget = !movingToTarget;
                 timer = 0.0f;
             }
+        }
+        if(movingWith && rigidbody2D != null)
+        {
+            rigidbody2D.velocity -= lastSpeed;
+            rigidbody2D.velocity += moveSpeed * new Vector2(direction.x, direction.y);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (movingWith)
+            rigidbody2D = collision.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(movingWith && rigidbody2D != null)
+        {
+            rigidbody2D.velocity -= lastSpeed;
+            rigidbody2D = null;
         }
     }
 }
